693e3e58423b668a135d1934b5a340c0f2999bd6
Muhammad Ridho K. Pratama <99ridho@gmail.com>
2019-09-26 10:16:07 +0700

    initialize view

---
+++ ContactApp/ContactList/Models/Contact.swift
@@ -0,0 +1,21 @@
+//
+//  Contact.swift
+//  ContactApp
+//
+//  Created by Ridho Pratama on 26/09/19.
+//  Copyright © 2019 Ridho Pratama. All rights reserved.
+//
+
+import Foundation
+
+struct Contact: Decodable {
+    let id: Int
+    let name: String
+    let phoneNumber: String
+    let email: String
+    let imageUrl: String
+}
+
+struct ContactListResponse: Decodable {
+    let data: [Contact]
+}
---
+++ ContactApp/ContactList/ViewController/ContactListViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ContactListViewController.swift
+//  ContactApp
+//
+//  Created by Ridho Pratama on 26/09/19.
+//  Copyright © 2019 Ridho Pratama. All rights reserved.
+//
+
+import UIKit
+
+class ContactListViewController: UIViewController {
+    private let tableView: UITableView = {
+        let tv = UITableView(frame: .zero, style: .plain)
+        tv.register(ContactListTableViewCell.self, forCellReuseIdentifier: ContactListTableViewCell.reuseIdentifier)
+        tv.estimatedRowHeight = 80
+        tv.rowHeight = 80
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupView()
+    }
+    
+    private func setupView() {
+        title = "Contact List"
+        
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+        
+        view.backgroundColor = .white
+    }
+}

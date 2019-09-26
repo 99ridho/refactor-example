4fe27c50250a04e908fdf72b713e1cd4ddbfcf38
Muhammad Ridho K. Pratama <99ridho@gmail.com>
2019-09-26 10:04:53 +0700

    first init

---
+++ ContactApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactApp
+//
+//  Created by Ridho Pratama on 26/09/19.
+//  Copyright © 2019 Ridho Pratama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

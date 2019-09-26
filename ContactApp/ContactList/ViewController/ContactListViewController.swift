3dd2aaf4781277e9c8851a37ef36e114e1f370a5
Muhammad Ridho K. Pratama <99ridho@gmail.com>
2019-09-26 10:25:56 +0700

    wiring url session to tableview cell

--- ContactApp/ContactList/ViewController/ContactListViewController.swift
+++ ContactApp/ContactList/ViewController/ContactListViewController.swift
@@ -19,6 +19,16 @@ class ContactListViewController: UIViewController {
         return tv
     }()
     
+    private let jsonDecoder: JSONDecoder = {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        return decoder
+    }()
+    
+    private var rawContacts: [Contact] = []
+    private var contactCellData: [ContactListCellData] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupView()
@@ -26,6 +36,9 @@ class ContactListViewController: UIViewController {
     
     private func setupView() {
         title = "Contact List"
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         
         view.addSubview(tableView)
         NSLayoutConstraint.activate([
@@ -36,5 +49,54 @@ class ContactListViewController: UIViewController {
         ])
         
         view.backgroundColor = .white
+    }
+    
+    private func fetchContacts() {
+        let url = URL(string: "https://gist.githubusercontent.com/99ridho/cbbeae1fa014522151e45a766492233c/raw/8935d40ae0650f12b452d6a5e9aa238a02b05511/contacts.json")!
+        let task = URLSession.shared.dataTask(with: url) { [weak self, jsonDecoder] (data, response, error) in
+            if error != nil {
+                // do something with error
+                return
+            }
+            
+            guard let theData = data else {
+                // do something when data is null
+                return
+            }
+            
+            do {
+                let response = try jsonDecoder.decode(ContactListResponse.self, from: theData)
+                let contacts = response.data
+                
+                self?.rawContacts = contacts
+                self?.contactCellData = contacts.map {
+                    ContactListCellData(imageURL: $0.imageUrl, name: $0.name)
+                }
+                
+                self?.tableView.reloadData()
+            } catch {
+                // do something when failed response mapping
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+extension ContactListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contactCellData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellData = contactCellData[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: ContactListTableViewCell.reuseIdentifier) as! ContactListTableViewCell
+        cell.configureCell(with: cellData)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
